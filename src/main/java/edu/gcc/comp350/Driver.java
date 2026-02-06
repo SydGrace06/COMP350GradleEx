62c3cf656375358d6fc231dfbedc702197bcb05e
SydGrace06 <sydgrace06@gmail.com>
2026-02-06 13:37:10 -0500

    Initial commit

---
+++ src/main/java/edu/gcc/comp350/Driver.java
@@ -0,0 +1,10 @@
+package edu.gcc.comp350;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("Hello Jar");
+        Calculator c = new Calculator();
+        System.out.println(c.add(4,5));
+        System.out.println("Test");
+    }
+}
